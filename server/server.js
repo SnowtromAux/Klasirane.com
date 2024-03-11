ba5855241819a7b7624022ffe45caf1fd1924ff7
lubod <lubo.daniel.dilov@gmail.com>
2024-03-11 04:01:52 +0200

    little fix

--- server/server.js
+++ server/server.js
@@ -4,19 +4,10 @@ const cors = require('cors');
 const app = express();
 const port = 3001;
 
-const allowedOrigins = ['http://15.188.118.216:3000'];
+//const allowedOrigins = ['http://15.188.118.216:3000'];
 
 // Use CORS middleware with options
-app.use(cors({
-  origin: function (origin, callback) {
-    // Check if the origin is in the allowedOrigins array or if it's undefined (no origin = same origin)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
 
 const comp_router = require('./routers/CompRouter.js');
 app.use("/competitions" , comp_router);
